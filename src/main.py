4feefb76411c86867854b9e729ee591958123db6
James Hotblack <james.c.hotblack@gmail.com>
2019-02-19 17:05:16 +0000

    Updated main.py to include a Hero class that can be initialized and rendered to the screen. Collaborators - Tom and James.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,50 @@
 import pygame
 
-HEIGHT = 600
-WIDTH = 800
+HEIGHT = 768
+WIDTH = 1024
 GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+class Hero(pygame.sprite.Sprite):
+    def __init__(self):
+
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((32, 32))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+ 
+ 
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Orthogonal Quest')
+
+all_sprites = pygame.sprite.Group()
+
+hero = Hero()  
+all_sprites.add(hero)
+      
+clock = pygame.time.Clock()
 
 running = True
 while running:
 
     for event in pygame.event.get():
-        print(event)
-        screen.fill(GREEN)
+        
+        #print(event)
+        screen.fill(WHITE)
+        all_sprites.update()
+    
+        # Key line of code for rendering sprites
+        all_sprites.draw(screen)
+ 
+        pygame.display.flip()
+ 
+        clock.tick(60)
+
         if event.type == pygame.QUIT:
             running = False
-
-    pygame.display.flip()
+ 
+pygame.quit()
