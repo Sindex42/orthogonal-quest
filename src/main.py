352e36a44c36c724aa781e11f621ba0ed1f14e1d
CKKH <chriskkhassan@gmail.com>
2019-02-19 16:50:55 +0000

    Refactor: extracted events method

--- src/main.py
+++ src/main.py
@@ -15,12 +15,16 @@ class Game:
         # game loop - set self.playing = False to end the game
         self.playing = True
         while self.playing:
-            for event in pg.event.get():
-                self.screen.fill(GREEN)
-                if event.type == pg.QUIT:
-                    self.playing = False
-
+            self.screen.fill(GREEN)
+            self.events()
             pg.display.flip()
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.playing = False
+
+
 
 # create instance of game
 game = Game()
