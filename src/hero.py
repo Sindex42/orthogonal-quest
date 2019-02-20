491408bda423cb93a413723bcae67d671c27ea28
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-20 15:48:58 +0000

    Made move up or down left or right private

--- src/hero.py
+++ src/hero.py
@@ -15,8 +15,8 @@ class Hero(pygame.sprite.Sprite):
     def handle_keys(self):
         """ Handles Keys """
         self.key = pygame.key.get_pressed()
-        self.move_up_or_down()
-        self.move_left_or_right()
+        self.__move_up_or_down()
+        self.__move_left_or_right()
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
@@ -26,13 +26,15 @@ class Hero(pygame.sprite.Sprite):
         if self.rect.top < 0:
             self.rect.top = 0
 
-    def move_up_or_down(self):
+    # private methods
+
+    def __move_up_or_down(self):
         if self.key[pygame.K_DOWN]: # down key
             self.rect.y += TILESIZE
         elif self.key[pygame.K_UP]: # up key
             self.rect.y -= TILESIZE
 
-    def move_left_or_right(self):
+    def __move_left_or_right(self):
         if self.key[pygame.K_RIGHT]: # right key
             self.rect.x += TILESIZE
         elif self.key[pygame.K_LEFT]: # left key
