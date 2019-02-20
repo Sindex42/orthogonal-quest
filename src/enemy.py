c96ea97dcc4dbe67b7c804e5fcf44233ffbfd7f6
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-20 13:50:28 +0000

    Fix some linter offences

--- src/enemy.py
+++ src/enemy.py
@@ -1,11 +1,19 @@
+"""
+Enemy module
+"""
+
 import pygame as pg
-from constants import *
+from constants import TILESIZE, RED
+
 
 class Enemy(pg.sprite.Sprite):
-    def __init__(self, x, y):
+    """
+    Creates enemies
+    """
+    def __init__(self, x_pos, y_pos):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.rect.x = TILESIZE * x
-        self.rect.y = TILESIZE * y
+        self.rect.x = TILESIZE * x_pos
+        self.rect.y = TILESIZE * y_pos
--- src/main.py
+++ src/main.py
@@ -3,45 +3,58 @@
 """
 
 import pygame as pg
-from enemy import *
-from constants import *
-
+from enemy import Enemy
+from constants import WIDTH, HEIGHT, TILESIZE, TITLE, BG_COLOUR, DARK_LINE
 
 class Game:
+    """
+    Setup and run game instance
+    """
+
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
 
     def run(self):
-        # game loop - set self.playing = False to end the game
+        ''' Game loop '''
+
         self.playing = True
         while self.playing:
             self.events()
             self.draw()
 
     def new(self):
+        ''' Creates sprites '''
+
         self.all_sprites = pg.sprite.Group()
         self.all_sprites.add(Enemy(1, 1))
 
     def draw_grid(self):
-       for x in range(0, WIDTH, TILESIZE):
-           pg.draw.line(self.screen, DARK_LINE, (x, 0), (x, HEIGHT))
-       for y in range(0, HEIGHT, TILESIZE):
-           pg.draw.line(self.screen, DARK_LINE, (0, y), (WIDTH, y))
+        ''' Draws the grid '''
+
+        for x_pos in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, DARK_LINE, (x_pos, 0), (x_pos, HEIGHT))
+        for y_pos in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, DARK_LINE, (0, y_pos), (WIDTH, y_pos))
 
     def draw(self):
-        self.screen.fill(BACKGROUND_COLOUR)
+        ''' Refreshes screen on every loop '''
+
+        self.screen.fill(BG_COLOUR)
         self.draw_grid()
         self.all_sprites.draw(self.screen)
         pg.display.flip()
 
     def events(self):
+        ''' Event listener '''
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.playing = False
 
+
 # create instance of game
-game = Game()
-game.new()
-game.run()
+GAME = Game()
+GAME.new()
+GAME.run()
