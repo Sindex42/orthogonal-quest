993e7541be8cd7038fae423d1998439746b2dc04
CKKH <chriskkhassan@gmail.com>
2019-02-22 12:44:22 +0000

    Feature: enermy sprite appears with texture

--- src/enemy.py
+++ src/enemy.py
@@ -10,8 +10,7 @@ class Enemy(pg.sprite.Sprite):
     def __init__(self, game, x_pos, y_pos):
         pg.sprite.Sprite.__init__(self)
         self.game = game
-        self.image = pg.Surface((TILESIZE - 1, TILESIZE -1 ))
-        self.image.fill(RED)
+        self.image = pg.transform.scale(pg.image.load('./images/enemy.png'), (TILESIZE -1, TILESIZE -1))
         self.rect = self.image.get_rect()
         self.x = x_pos
         self.y = y_pos
