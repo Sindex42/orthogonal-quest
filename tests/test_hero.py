eef096deed3f23667164839177c18aa9da0418a8
Michael Nguyen <michaelnguyen974@gmail.com>
2019-02-22 14:42:33 +0000

    Added pydoubles and pycov to requirements.txt

--- tests/test_hero.py
+++ tests/test_hero.py
@@ -1,30 +1,22 @@
 import pygame
 import pytest
 
-from pynput.keyboard import Key, Controller
-
+# from pynput.keyboard import Key, Controller
+from doubles import InstanceDouble, allow
 from src.hero import Hero
+
 
 class TestHero(object):
 
-  #Testing initial position
-  def test_rect_x_position(self):
-    hero = Hero()
-    assert hero.rect.x == 240
-
-  def test_rect_y_position(self):
-    hero = Hero()
-    assert hero.rect.y == 176
+ 
 
   #Testing hero movement
   def test_rect_new_position(self):
-    pygame.init()
-    pygame.display.set_mode((1024, 768))
-    keyboard = Controller()
-    keyboard.press(Key.down)
-    hero = Hero()
-    hero.handle_keys()
-    
-    assert hero.rect.y == 367
+    game = ''
+    hero = Hero(game, 0, 0)
+    assert(hero.x) == 0
+    assert(hero.y) == 0
+
+  
 
   
