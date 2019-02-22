c5e2de874132bb75eaa7eff721982eb8f8be206d
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-22 17:39:59 +0000

    Pass linter

--- src/hero.py
+++ src/hero.py
@@ -1,7 +1,7 @@
 ''' Hero module '''
 
-import pygame as pg
 import os
+import pygame as pg
 
 from constants import TILESIZE
 
@@ -13,33 +13,38 @@ class Hero(pg.sprite.Sprite):
 
         pg.sprite.Sprite.__init__(self)
         self.game = game
-        self.x = x_pos
-        self.y = y_pos
+        self.x_pos = x_pos
+        self.y_pos = y_pos
         self.link_animation_setup()
-        self.image = pg.transform.scale(pg.image.load('./images/link/link_down/link_f0.png'), (TILESIZE -1, TILESIZE -1))
+        self.image = pg.transform.scale(pg.image.load(
+            './images/link/link_down/link_f0.png'), (TILESIZE - 1, TILESIZE - 1))
         self.rect = self.image.get_rect()
+        self.right_index = self.left_index = self.up_index = self.down_index = 0
 
-    def move(self, dx=0, dy=0):
-        if not self.collide_with_walls(dx, dy) and not self.collide_with_enemy(dx, dy):
-            self.x += dx
-            self.y += dy
-        #Changes link image on each arrow key push
-        if dx == 1:
+    def move(self, d_x=0, d_y=0):
+        ''' Defines hero movement '''
+
+        if not self.collide_with_walls(d_x, d_y) and not self.collide_with_enemy(d_x, d_y):
+            self.x_pos += d_x
+            self.y_pos += d_y
+
+        # Changes link image on each arrow key push
+        if d_x == 1:
             self.right_index += 1
             if self.right_index >= len(self.right_images):
                 self.right_index = 0
             self.image = self.right_images[self.right_index]
-        if dx == -1:
+        if d_x == -1:
             self.left_index += 1
             if self.left_index >= len(self.left_images):
                 self.left_index = 0
             self.image = self.left_images[self.left_index]
-        if dy == 1:
+        if d_y == 1:
             self.down_index += 1
             if self.down_index >= len(self.down_images):
                 self.down_index = 0
             self.image = self.down_images[self.down_index]
-        if dy == -1: 
+        if d_y == -1:
             self.up_index += 1
             if self.up_index >= len(self.up_images):
                 self.up_index = 0
@@ -62,6 +67,8 @@ class Hero(pg.sprite.Sprite):
         return False
 
     def link_animation_setup(self):
+        ''' Loops through index arrays and correct sprite image load methods '''
+
         self.up_index = self.right_index = self.down_index = self.left_index = 0
         self.up_images = []
         self.load_up_image()
@@ -73,24 +80,40 @@ class Hero(pg.sprite.Sprite):
         self.load_left_image()
 
     def load_up_image(self):
+        ''' Loads upward facing sprites '''
+
         for image in os.listdir('images/link/link_up'):
-            path = os.path.join('images/link/link_up', image) 
-            self.up_images.append(pg.transform.scale(pg.image.load(path), (TILESIZE -1, TILESIZE -1)))
-     
+            path = os.path.join('images/link/link_up', image)
+            self.up_images.append(
+                pg.transform.scale(
+                    pg.image.load(path), (TILESIZE - 1, TILESIZE - 1)))
+
     def load_right_image(self):
+        ''' Loads rightward facing sprites '''
+
         for image in os.listdir('images/link/link_right'):
             path = os.path.join('images/link/link_right', image)
-            self.right_images.append(pg.transform.scale(pg.image.load(path), (TILESIZE -1, TILESIZE -1)))
+            self.right_images.append(
+                pg.transform.scale(
+                    pg.image.load(path), (TILESIZE - 1, TILESIZE - 1)))
 
     def load_down_image(self):
+        ''' Loads downward facing sprites '''
+
         for image in os.listdir('images/link/link_down'):
             path = os.path.join('images/link/link_down', image)
-            self.down_images.append(pg.transform.scale(pg.image.load(path), (TILESIZE -1, TILESIZE -1)))
+            self.down_images.append(
+                pg.transform.scale(
+                    pg.image.load(path), (TILESIZE - 1, TILESIZE - 1)))
 
     def load_left_image(self):
+        ''' Loads leftward facing sprites '''
+
         for image in os.listdir('images/link/link_left'):
             path = os.path.join('images/link/link_left', image)
-            self.left_images.append(pg.transform.scale(pg.image.load(path), (TILESIZE -1, TILESIZE -1)))
+            self.left_images.append(
+                pg.transform.scale(
+                    pg.image.load(path), (TILESIZE - 1, TILESIZE - 1)))
 
     def update(self):
         ''' Update position '''
