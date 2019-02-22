747a053cd0a4fd5cf0407ed0e10a566b73b4b71e
James Hotblack <james.c.hotblack@gmail.com>
2019-02-22 15:07:44 +0000

    Further Refactoring

--- src/hero.py
+++ src/hero.py
@@ -8,23 +8,9 @@ class Hero(pg.sprite.Sprite):
 
         pg.sprite.Sprite.__init__(self)
         self.game = game
-        #self.image = pg.transform.scale(pg.image.load('./images/link_f0.png'), (TILESIZE -1, TILESIZE -1))
         self.x = x_pos
         self.y = y_pos
-
-        self.up_index = 0
-        self.up_images = []
-        self.load_up_image()
-        self.right_index = 0
-        self.right_images = []
-        self.load_right_image()
-        self.down_index = 0
-        self.down_images = []
-        self.load_down_image()
-        self.left_index = 0
-        self.left_images = []
-        self.load_left_image()
-
+        self.link_animation_setup()
         self.image = pg.transform.scale(pg.image.load('./images/link/link_down/link_f0.png'), (TILESIZE -1, TILESIZE -1))
         self.rect = self.image.get_rect()
 
@@ -34,13 +20,11 @@ class Hero(pg.sprite.Sprite):
             self.y += dy
         #Changes link image on each arrow key push
         if dx == 1:
-            #self.image = pg.transform.scale(pg.image.load('./images/link_r0.png'), (TILESIZE -1, TILESIZE -1))
             self.right_index += 1
             if self.right_index >= len(self.right_images):
                 self.right_index = 0
             self.image = self.right_images[self.right_index]
         if dx == -1:
-            #self.image = pg.transform.scale(pg.image.load('./images/link_l0.png'), (TILESIZE -1, TILESIZE -1))
             self.left_index += 1
             if self.left_index >= len(self.left_images):
                 self.left_index = 0
@@ -50,14 +34,11 @@ class Hero(pg.sprite.Sprite):
             if self.down_index >= len(self.down_images):
                 self.down_index = 0
             self.image = self.down_images[self.down_index]
-        if dy == -1:
-            #self.image = pg.transform.scale(pg.image.load('./images/link_b0.png'), (TILESIZE -1, TILESIZE -1))    
+        if dy == -1: 
             self.up_index += 1
             if self.up_index >= len(self.up_images):
                 self.up_index = 0
             self.image = self.up_images[self.up_index]
-            print(self.up_index)
-
 
     def collide_with_walls(self, dx=0, dy=0):
         for wall in self.game.walls_sprites:
@@ -72,6 +53,20 @@ class Hero(pg.sprite.Sprite):
                 print("Enemy collision")
                 return True
         return False
+
+    def link_animation_setup(self):
+        self.up_index = 0
+        self.up_images = []
+        self.load_up_image()
+        self.right_index = 0
+        self.right_images = []
+        self.load_right_image()
+        self.down_index = 0
+        self.down_images = []
+        self.load_down_image()
+        self.left_index = 0
+        self.left_images = []
+        self.load_left_image()
 
     def load_up_image(self):
         for image in os.listdir('images/link/link_up'):
@@ -92,7 +87,6 @@ class Hero(pg.sprite.Sprite):
         for image in os.listdir('images/link/link_left'):
             path = os.path.join('images/link/link_left', image)
             self.left_images.append(pg.transform.scale(pg.image.load(path), (TILESIZE -1, TILESIZE -1)))
-
 
     def update(self):
         self.rect.x = self.x * TILESIZE + 1
