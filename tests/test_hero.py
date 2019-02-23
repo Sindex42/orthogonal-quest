1d0e34f4b64d6c0d1df7bcb7cefab64fd510d846
Michael Nguyen <michaelnguyen974@gmail.com>
2019-02-23 13:36:04 +0000

    fakegame.py, tried instance doubles, failing second test

--- tests/test_hero.py
+++ tests/test_hero.py
@@ -1,20 +1,26 @@
-import pygame
 import pytest
-
-# from pynput.keyboard import Key, Controller
-from doubles import InstanceDouble, allow
+from unittest.mock import Mock
 from src.hero import Hero
 
+from doubles import InstanceDouble, allow
 
 class TestHero(object):
 
- 
 
   #Testing hero movement
-  def test_rect_new_position(self):
+  def test_hero_starting_position(self):
     game = ''
     hero = Hero(game, 0, 0)
     assert(hero.x) == 0
+    assert(hero.y) == 0
+  
+  def test_hero_movement(self):
+    Game2 = InstanceDouble('src.fakegame.Game2')
+    allow(Game2).walls_sprites 
+    hero = Hero(Game2, 0, 0)
+    hero.move(1, 0)
+    # hero.collide_with_walls = False 
+    assert(hero.x) == 1
     assert(hero.y) == 0
 
   
