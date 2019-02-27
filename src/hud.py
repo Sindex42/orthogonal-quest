a377925d52e0c3063f2f8d141e856da3de009ecc
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-27 11:36:46 +0000

    Extract hud and info_screens

---
+++ src/hud.py
@@ -0,0 +1,22 @@
+''' HUD module '''
+
+import pygame as pg
+from constants import BAR_HEIGHT, BAR_LENGTH, WHITE, GREEN, YELLOW, RED
+
+
+def draw_hero_health(surf, x_pos, y_pos, pct):
+    ''' Draws Hero Health Bar'''
+
+    if pct < 0:
+        pct = 0
+    fill = pct * BAR_LENGTH
+    outline_rect = pg.Rect(x_pos, y_pos, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pg.Rect(x_pos, y_pos, fill, BAR_HEIGHT)
+    if pct > 0.6:
+        col = GREEN
+    elif pct > 0.3:
+        col = YELLOW
+    else:
+        col = RED
+    pg.draw.rect(surf, col, fill_rect)
+    pg.draw.rect(surf, WHITE, outline_rect, 2)
---
+++ src/info_screens.py
@@ -0,0 +1,48 @@
+''' Start and end screens '''
+
+import pygame as pg
+from constants import BG_COLOUR, TITLE, WIDTH, HEIGHT, GOLD, FONT_NAME
+
+
+def draw_text_on_screen(game, text, size, x_pos, y_pos):
+    ''' Draws text on screen '''
+    font = pg.font.Font(pg.font.match_font(FONT_NAME), size)
+    text_surface = font.render(text, True, GOLD)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x_pos, y_pos)
+    game.screen.blit(text_surface, text_rect)
+
+def show_start_screen(game):
+    ''' Shows the start screen '''
+
+    game.screen.fill(BG_COLOUR)
+    draw_text_on_screen(game, TITLE, 48, WIDTH / 2, HEIGHT / 4)
+    draw_text_on_screen(
+        game,
+        "Use 'w/a/s/d' keys to move and arrow keys to attack in their direction",
+        22,
+        WIDTH / 2,
+        HEIGHT / 2)
+    draw_text_on_screen(
+        game,
+        "Press any key to play",
+        22,
+        WIDTH / 2,
+        HEIGHT * 3 / 4)
+    pg.display.flip()
+
+def show_end_screen(game):
+    ''' Shows the end screen '''
+
+    game.enemy_sprites.empty()
+    game.all_sprites.empty()
+    game.walls_sprites.empty()
+    game.screen.fill(BG_COLOUR)
+    draw_text_on_screen(game, "GAME OVER", 40, WIDTH / 2, HEIGHT / 2)
+    draw_text_on_screen(
+        game,
+        "Press a key to play again",
+        22,
+        WIDTH / 2,
+        HEIGHT * 3 / 4)
+    pg.display.flip()
